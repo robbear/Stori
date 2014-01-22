955e82500d4ae2311c99fc9baff9052aea8a467d
Rob Bearman <robbear@hyperfine.com>
2014-01-21 18:27:27 -0800

    Fix build, adding import of com.stori.stori.Config in AWSS3Provider to account for new use of AWS_BUCKET_NAME

--- android/Stori/src/main/java/com/stori/stori/cloudproviders/AWSS3Provider.java
+++ android/Stori/src/main/java/com/stori/stori/cloudproviders/AWSS3Provider.java
@@ -10,6 +10,7 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.stori.stori.Config;
 import com.stori.stori.EditPlayActivity;
 import com.stori.stori.Utilities;
 
