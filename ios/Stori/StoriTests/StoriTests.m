8cde3f778bf79a389d56c00fcac616443730dba3
Rob Bearman <robbear@hyperfine.com>
2014-03-05 15:59:50 -0800

    Added more unit tests for SlideShareJSON

--- ios/Stori/StoriTests/StoriTests.m
+++ ios/Stori/StoriTests/StoriTests.m
@@ -37,7 +37,15 @@ SlideShareJSON *_ssj;
     _ssj = [[SlideShareJSON alloc] initWithString:jsonString];
     HFLogDebug(@"ssj.getJsonDictionary: %@", [_ssj toString]);
     
-    //XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    NSString *title = [_ssj getTitle];
+    if (![title isEqualToString:@"My Stori"]) {
+        XCTFail(@"Failed to get the correct title from the SlideShareJSON object");
+    }
+    
+    NSString *description = [_ssj getDescription];
+    if (![description isEqualToString:@"My description"]) {
+        XCTFail(@"Failed to get the correct description from the SlideShareJSON object");
+    }
 }
 
 - (void)testCreateMultipleSlides {
@@ -55,6 +63,11 @@ SlideShareJSON *_ssj;
         [_ssj upsertSlideWithSlideId:slideUuid atIndex:i withImageUrl:imageUrl withAudioUrl:audioUrl withSlideText:nil forceNulls:TRUE];
     }
     HFLogDebug(@"ssj.getJsonDictionary: %@", [_ssj toString]);
+    
+    int count = [_ssj getSlideCount];
+    if (count != 5) {
+        XCTFail(@"Slide count should be 5. Instead, it is %d", count);
+    }
 }
 
 - (void)testGetSlideBySlideId {
@@ -69,6 +82,10 @@ SlideShareJSON *_ssj;
     NSString *audioFilename = [sj getAudioFilename];
     NSString *slideText = [sj getText];
     HFLogDebug(@"Values: imageUrl=%@, audioUrl=%@, imageFilename=%@, audioFilename=%@, slideText=%@", imageUrl, audioUrl, imageFilename, audioFilename, slideText);
+    
+    if (!imageUrl || !audioUrl || !imageFilename || !audioFilename) {
+        XCTFail("Failed to retrieve slide image and/or audio strings");
+    }
 }
 
 - (void)testGetOrderIndexForSlide {
@@ -78,6 +95,10 @@ SlideShareJSON *_ssj;
     
     int index = [_ssj getOrderIndexForSlide:_testSlideUuid];
     HFLogDebug(@"Order index: %d", index);
+    
+    if (index != 1) {
+        XCTFail("Failed to retrieve index as 1. Instead, we got index of %d", index);
+    }
 }
 
 - (void)testGetSlideAtIndex {
@@ -92,6 +113,81 @@ SlideShareJSON *_ssj;
     NSString *audioFilename = [sj getAudioFilename];
     NSString *slideText = [sj getText];
     HFLogDebug(@"Values: imageUrl=%@, audioUrl=%@, imageFilename=%@, audioFilename=%@, slideText=%@", imageUrl, audioUrl, imageFilename, audioFilename, slideText);
+
+    if (!imageUrl || !audioUrl || !imageFilename || !audioFilename) {
+        XCTFail("Failed to retrieve slide image and/or audio strings");
+    }
+}
+
+- (void)testUpserSlide {
+    [self testCreateMultipleSlides];
+    
+    NSString *urlString = @"http://hyperfine.com/foo.jpg";
+    [_ssj upsertSlideWithSlideId:_testSlideUuid atIndex:1 withImageUrl:urlString withAudioUrl:nil withSlideText:nil forceNulls:FALSE];
+    SlideJSON *sj = [_ssj getSlideBySlideId:_testSlideUuid];
+    
+    NSString *imageUrl = [sj getImageUrlString];
+    if (![imageUrl isEqualToString:urlString]) {
+        XCTFail("Failed to retrieve modified slide");
+    }
+    
+    NSString *audioUrl = [sj getAudioUrlString];
+    if (!audioUrl) {
+        XCTFail("Failed to properly upsert slide. The audio url was overwritten to null");
+    }
+    
+    NSString *slideText = [sj getText];
+    if (!(slideText == (id)[NSNull null] || slideText.length == 0)) {
+        XCTFail("Failed to retrieve nil slideText");
+    }
+
+    [_ssj upsertSlideWithSlideId:_testSlideUuid atIndex:1 withImageUrl:nil withAudioUrl:audioUrl withSlideText:nil forceNulls:TRUE];
+    sj = [_ssj getSlideBySlideId:_testSlideUuid];
+    imageUrl = [sj getImageUrlString];
+    audioUrl = [sj getAudioUrlString];
+    slideText = [sj getText];
+    
+    if (!(imageUrl == (id)[NSNull null] || imageUrl.length == 0)) {
+        XCTFail("Failed to properly overwrite imageUrl with nil in upsert with forceNulls true");
+    }
+    if (audioUrl == (id)[NSNull null] || audioUrl.length == 0) {
+        XCTFail("Failed to properly upsert the slide, with audio url being overwritten");
+    }
+    if (!(slideText == (id)[NSNull null] || slideText.length == 0)) {
+        XCTFail("Failed to retrieve nil slideText");
+    }
+}
+
+- (void)testReorder {
+    [self testCreateMultipleSlides];
+    
+    [_ssj reorderWithCurrentPosition:1 atNewPosition:0];
+    NSString *slideUuid = [_ssj getSlideUuidByOrderIndex:0];
+    if (![slideUuid isEqualToString:_testSlideUuid]) {
+        XCTFail("Failed to move slide index 1 to index 0");
+    }
+    
+    [_ssj reorderWithCurrentPosition:0 atNewPosition:2];
+    slideUuid = [_ssj getSlideUuidByOrderIndex:2];
+    if (![slideUuid isEqualToString:_testSlideUuid]) {
+        XCTFail("Failed to move slide index 0 to index 2");
+    }
+    
+    [_ssj reorderWithCurrentPosition:2 atNewPosition:4];
+    slideUuid = [_ssj getSlideUuidByOrderIndex:4];
+    if (![slideUuid isEqualToString:_testSlideUuid]) {
+        XCTFail("Failed to move slide index 0 to index 2");
+    }
+}
+
+- (void)testRemoveSlide {
+    [self testCreateMultipleSlides];
+    
+    [_ssj removeSlideBySlideId:_testSlideUuid];
+    int count = [_ssj getSlideCount];
+    if (count != 4) {
+        XCTFail("Failed to remove slide");
+    }
 }
 
 @end
